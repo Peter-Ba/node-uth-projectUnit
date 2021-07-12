44023d31afd5993d602222516e5884eb0145a1ea
jasonligg <9956289+jasonligg@users.noreply.github.com>
2021-07-12 16:20:09 -0400

    update file paths to common directory structure; formatted with prettier

--- workerThreads/main.js
+++ workerThreads/main.js
@@ -1,27 +1,31 @@
-const { Worker } = require('worker_threads')
+const { Worker } = require('worker_threads');
 
 const workerFile = './workerThreads/writeToFile.js';
 
-const readFileName = './data/large-text.txt';
+const readFileName = './data/large.txt';
 
-const writeFileNames = ['./workerThreads/writeFiles/write-file-1.txt', 
-                        './workerThreads/writeFiles/write-file-2.txt', 
-                        './workerThreads/writeFiles/write-file-3.txt', 
-                        './workerThreads/writeFiles/write-file-4.txt', 
-                        './workerThreads/writeFiles/write-file-5.txt']; 
+const writeFileNames = [
+  './output/a_to_e.txt',
+  './output/e_to_i.txt',
+  './output/i_to_o.txt',
+  './output/o_to_u.txt',
+  './output/u_to_a.txt',
+];
 
-const transformOptions = [{ searchValue: /a/gim, replaceValue: 'e' },
-                            { searchValue: /e/gim, replaceValue: 'i' },
-                            { searchValue: /i/gim, replaceValue: 'o' },
-                            { searchValue: /o/gim, replaceValue: 'u' },
-                            { searchValue: /u/gim, replaceValue: 'a' }];
+const transformOptions = [
+  { searchValue: /a/gim, replaceValue: 'e' },
+  { searchValue: /e/gim, replaceValue: 'i' },
+  { searchValue: /i/gim, replaceValue: 'o' },
+  { searchValue: /o/gim, replaceValue: 'u' },
+  { searchValue: /u/gim, replaceValue: 'a' },
+];
 
 for (let i = 0; i < writeFileNames.length; i++) {
-    const writeFileName = writeFileNames[i];
-    const transformOption = transformOptions[i];
-    const workerData = { readFileName, writeFileName, transformOption };
-    
-    const worker = new Worker(workerFile, { workerData })
-    worker.on('message', (({ message }) => console.log(message)))
-    worker.on('error', console.error);
+  const writeFileName = writeFileNames[i];
+  const transformOption = transformOptions[i];
+  const workerData = { readFileName, writeFileName, transformOption };
+
+  const worker = new Worker(workerFile, { workerData });
+  worker.on('message', ({ message }) => console.log(message));
+  worker.on('error', console.error);
 }
